57917b9a065e55dabdf9664491212693bb5f7142
Max Nazaruk <84872903+MaxNazaruk-Developer@users.noreply.github.com>
2021-09-04 11:49:22 +0300

    Fix code task 3

--- force-app/main/default/lwc/contactTableNameAndIDname/contactTableNameAndIDname.js
+++ force-app/main/default/lwc/contactTableNameAndIDname/contactTableNameAndIDname.js
@@ -1,20 +1,12 @@
-import { LightningElement, wire, api, track } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import getContactNameAndId from '@salesforce/apex/contactTableNameAndIdNameController.getContactNameAndId';
-import { refreshApex } from '@salesforce/apex';
-import { updateRecord } from 'lightning/uiRecordApi';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-
-import NAME from '@salesforce/schema/Contact.Name';
-import ID from '@salesforce/schema/Contact.Id';
 
 const COLUM = [
     { label: 'IdName', fieldName: 'IdNameName' },    
     { label: 'Name', fieldName: 'LinkName', type: 'url', typeAttributes: {label: {fieldName: 'Name'}}}
 ];
-export default class ContactTableNameAndIDname extends LightningElement {
-    @api recordId;
-    columns = COLUM;
-    draftValues = [];
+export default class ContactTableNameAndIDname extends LightningElement {    
+    columns = COLUM;    
     @track data = [];
     contacts;
 
@@ -38,39 +30,5 @@ export default class ContactTableNameAndIDname extends LightningElement {
             this.error = error;
             this.data = undefined;
         } 
-    }
-    handleSave(event){
-
-        const fields = {}; 
-        fields[NAME.fieldApiName] = event.detail.draftValues[0].Name;               
-        fields[ID.fieldApiName] = event.detail.draftValues[0].Id;
-
-        const recordInput = {fields};        
-
-        updateRecord(recordInput)
-        .then(() => {
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Success',
-                    message: 'Lead updated',
-                    variant: 'success'
-                })
-            );
-            
-            return refreshApex(this.contacts).then(() => {
-                
-                this.draftValues = [];
-
-            });
-        }).catch(error => {
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Error updating or reloading record',
-                    message: error.body.message,
-                    variant: 'error'
-                })
-            );
-        });
-
-    }
+    }    
 }
